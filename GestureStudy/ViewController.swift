7bdd7fd44ce69f3440c1b6e6c9561ce19ac3eff8
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-06-10 08:56:03 +0300

    Initial Commit

---
+++ GestureStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestureStudy
+//
+//  Created by Ömer Faruk Kılıçaslan on 10.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
